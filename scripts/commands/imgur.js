f47afc23cdde261681858a0ef3ac298c76b5ed2e
badolkhan646 <141847046+badolkhan646@users.noreply.github.com>
2024-03-29 23:47:29 +0600

    Update imgur.js

--- scripts/commands/imgur.js
+++ scripts/commands/imgur.js
@@ -1,30 +1,46 @@
+const axios = require('axios');
+
 module.exports.config = {
   name: "imgur",
   version: "1.0.0",
   permission: 0,
-  credits: "Nayan",
+  credits: "BADOL-KHAN",
   description: "",
-  prefix: true, 
-  category: "user", 
+  prefix: true,
+  category: "user",
   usages: "Link",
   cooldowns: 5,
-  dependencies: {
-  }
+  dependencies: {}
 };
 
 module.exports.run = async ({ api, event, args }) => {
-    const axios = global.nodemodule['axios'];
-  const apis = await axios.get('https://raw.githubusercontent.com/MR-NAYAN-404/NAYAN-BOT/main/api.json')
-  const n = apis.data.api
-    const linkanh = event.messageReply.attachments[0].url || args.join(" ");
-    if (!linkanh)
-        return api.sendMessage('[âšœï¸]âžœ Please give feedback or enter the image or vide link', event.threadID, event.messageID);
-    try {
-      var tpk = `",`;
-        const allPromise = (await Promise.all(event.messageReply.attachments.map(item => axios.get(`${n}/imgurv2?link=${encodeURIComponent(item.url)}`)))).map(item => item.data.uploaded.image);
-        return api.sendMessage(`"` + allPromise.join('"\n"') + tpk, event.threadID, event.messageID);
+  try {
+    const links = [
+      "https://i.imgur.com/WXxs2nL.jpeg",
+      "https://i.imgur.com/WXxs2nL.jpeg"
+    ];
+    const randomLink = links[Math.floor(Math.random() * links.length)];
+
+    const response = await axios.get(randomLink, { responseType: 'stream' });
+    const attachment = response.data;
+
+    const apis = await axios.get('https://raw.githubusercontent.com/MR-NAYAN-404/NAYAN-BOT/main/api.json');
+    const n = apis.data.api;
+
+    const linkanh = event.messageReply.attachments[0]?.url || args.join(" ");
+    if (!linkanh) {
+      return api.sendMessage('[⚠️] Please provide an image or video link.', event.threadID, event.messageID);
     }
-    catch (e) {
-        return api.sendMessage('[âšœï¸]âžœ An error occurred while executing the command', event.threadID, event.messageID);
-    }
+
+    const allPromise = await Promise.all(event.messageReply.attachments.map(async (item) => {
+      const response = await axios.get(`${n}/imgurv2?link=${encodeURIComponent(item.url)}`);
+      return response.data.uploaded.image;
+    }));
+
+    const message = `"${allPromise.join('"\n"')}"`;
+    api.sendMessage({ body: message, attachment: attachment }, event.threadID, event.messageID);
+  } catch (error) {
+    console.error(error);
+    api.sendMessage('[⚠️] An error occurred while executing the command.', event.threadID, event.messageID);
+  }
 };
